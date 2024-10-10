6a42fcd26d475c81c63e2a80d5450292a715da9e
fxex <hugoantoniofrey2003@gmail.com>
2024-10-10 17:57:48 -0300

    BD: Se modificaron los nombres de los id de las tablas correspondiente al uargflow

--- Implementación/Entregable VestaRiskManager/bdvestariskmanager.sql
+++ Implementación/Entregable VestaRiskManager/bdvestariskmanager.sql
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `permiso` (
-  `id` int(11) NOT NULL,
+  `id_permiso` int(11) NOT NULL,
   `nombre` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
@@ -50,7 +50,7 @@ INSERT INTO `permiso` (`id`, `nombre`) VALUES
 --
 
 CREATE TABLE `rol` (
-  `id` int(11) NOT NULL,
+  `id_rol` int(11) NOT NULL,
   `nombre` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
@@ -92,7 +92,7 @@ INSERT INTO `rol_permiso` (`id_rol`, `id_permiso`) VALUES
 --
 
 CREATE TABLE `usuario` (
-  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
   `nombre` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
@@ -134,15 +134,15 @@ INSERT INTO `usuario_rol` (`id_usuario`, `id_rol`) VALUES
 -- Indices de la tabla `permiso`
 --
 ALTER TABLE `permiso`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `ID_PERMISO_IND` (`id`);
+  ADD PRIMARY KEY (`id_permiso`),
+  ADD UNIQUE KEY `ID_PERMISO_IND` (`id_permiso`);
 
 --
 -- Indices de la tabla `rol`
 --
 ALTER TABLE `rol`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `ID_ROL_IND` (`id`);
+  ADD PRIMARY KEY (`id_rol`),
+  ADD UNIQUE KEY `ID_ROL_IND` (`id_rol`);
 
 --
 -- Indices de la tabla `rol_permiso`
@@ -157,9 +157,9 @@ ALTER TABLE `rol_permiso`
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id_usuario`),
   ADD UNIQUE KEY `UN_USUARIO` (`email`,`nombre`),
-  ADD UNIQUE KEY `ID_USUARIO_IND` (`id`);
+  ADD UNIQUE KEY `ID_USUARIO_IND` (`id_usuario`);
 
 --
 -- Indices de la tabla `usuario_rol`
@@ -178,19 +178,19 @@ ALTER TABLE `usuario_rol`
 -- AUTO_INCREMENT de la tabla `permiso`
 --
 ALTER TABLE `permiso`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_permiso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `rol`
 --
 ALTER TABLE `rol`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- Restricciones para tablas volcadas
@@ -200,15 +200,15 @@ ALTER TABLE `usuario`
 -- Filtros para la tabla `rol_permiso`
 --
 ALTER TABLE `rol_permiso`
-  ADD CONSTRAINT `fk_rol_permiso_permiso` FOREIGN KEY (`id_permiso`) REFERENCES `permiso` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_rol_permiso_rol` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_rol_permiso_permiso` FOREIGN KEY (`id_permiso`) REFERENCES `permiso` (`id_permiso`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_rol_permiso_rol` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id_rol`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `usuario_rol`
 --
 ALTER TABLE `usuario_rol`
-  ADD CONSTRAINT `fk_usuario_rol_rol` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_usuario_rol_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_usuario_rol_rol` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id_rol`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_usuario_rol_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
