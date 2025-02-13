ca4af92f19e2195dee16598cd645a398dbef8503
fxex <hugoantoniofrey2003@gmail.com>
2025-02-13 15:31:29 -0300

    Feat: Se integro una libreria para la generacion de informes

--- Implementación/Entregable VestaRiskManager/Frontend/VestaRiskManager/src/utils/fecha.js
+++ Implementación/Entregable VestaRiskManager/Frontend/VestaRiskManager/src/utils/fecha.js
@@ -10,6 +10,26 @@ export const formatearFecha = (fecha) => {
 };
 
 /**
+ * Convierte una fecha en formato YYYY-MM-DD a DD/MM/YYYY.
+ *
+ * @param {Date} fecha
+ * @returns {string} - La fecha en formato DD/MM/YYYY HH:MM.
+ */
+export const formatearFechaHora = (fecha) => {
+  // Extraer día, mes y año
+  const day = String(fecha.getDate()).padStart(2, "0"); // Asegura dos dígitos
+  const month = String(fecha.getMonth() + 1).padStart(2, "0"); // Los meses van de 0 a 11
+  const year = fecha.getFullYear();
+
+  // Extraer horas y minutos
+  const hours = String(fecha.getHours()).padStart(2, "0");
+  const minutes = String(fecha.getMinutes()).padStart(2, "0");
+
+  // Formatear la fecha y hora
+  return `${day}/${month}/${year} ${hours}:${minutes}`;
+};
+
+/**
  * Comprueba si la fecha de inicio es anterior a la fecha de fin.
  *
  * @param {string} fecha_inicio - La fecha de inicio en formato YYYY-MM-DD.
