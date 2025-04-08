4dd32ce74f51067225f98be7de2f20e9066c8682
Hugo Frey <hugoantoniofrey2003@gmail.com>
2025-04-08 09:56:01 -0300

    Fix: Se modifico la redaccion de la descripcion de la categoria

--- Implementación/Entregable VestaRiskManager/datos basicos.sql
+++ Implementación/Entregable VestaRiskManager/datos basicos.sql
@@ -1,14 +1,14 @@
 INSERT INTO categoria (nombre, descripcion) VALUES
-('Compromiso del Cliente', 'Riesgo asociado al nivel de involucramiento y apoyo del cliente en el proyecto, lo que puede afectar su desarrollo y entrega.'),
-('Definición del Cliente', 'Riesgo derivado de la claridad y estabilidad de los requerimientos del cliente, pudiendo generar cambios inesperados y costos adicionales.'),
-('Cronograma', 'Riesgo relacionado con retrasos en la planificación y ejecución del proyecto, afectando entregas y costos.'),
-('Experiencia y Capacidad', 'Riesgo que surge por la falta de conocimientos, habilidades o experiencia del equipo, lo que puede comprometer la calidad y el éxito del proyecto.'),
-('Duración y Tamaño', 'Riesgo asociado a la magnitud y tiempo de ejecución del proyecto, donde una mayor duración o tamaño incrementa la probabilidad de imprevistos.'),
-('Legal y Contractual', 'Riesgo vinculado a aspectos legales, regulaciones y cumplimiento de contratos, lo que puede generar disputas o sanciones.'),
-('Tecnología', 'Riesgo derivado del uso de tecnologías nuevas o inestables, que pueden generar fallos, incompatibilidades o problemas de implementación.'),
-('Complejidad', 'Riesgo asociado al grado de dificultad del proyecto en términos técnicos, organizativos o de integración con otros sistemas.'),
-('Aspectos Financieros', 'Riesgo relacionado con la disponibilidad y gestión de recursos financieros, afectando la viabilidad y sostenibilidad del proyecto.'),
-('Subcontratistas', 'Riesgo derivado de la dependencia de terceros para la ejecución de partes del proyecto, lo que puede afectar la calidad, tiempos y costos.');
+('Compromiso del Cliente', 'Riesgos asociados al nivel de involucramiento y apoyo del cliente en el proyecto, lo que puede afectar su desarrollo y entrega.'),
+('Definición del Cliente', 'Riesgos derivados de la claridad y estabilidad de los requerimientos del cliente, pudiendo generar cambios inesperados y costos adicionales.'),
+('Cronograma', 'Riesgos relacionados con retrasos en la planificación y ejecución del proyecto, afectando entregas y costos.'),
+('Experiencia y Capacidad', 'Riesgos que surgen por la falta de conocimientos, habilidades o experiencia del equipo, lo que puede comprometer la calidad y el éxito del proyecto.'),
+('Duración y Tamaño', 'Riesgos asociados a la magnitud y tiempo de ejecución del proyecto, donde una mayor duración o tamaño incrementa la probabilidad de imprevistos.'),
+('Legal y Contractual', 'Riesgos vinculados a aspectos legales, regulaciones y cumplimiento de contratos, lo que puede generar disputas o sanciones.'),
+('Tecnología', 'Riesgos derivados del uso de tecnologías nuevas o inestables, que pueden generar fallos, incompatibilidades o problemas de implementación.'),
+('Complejidad', 'Riesgos asociados al grado de dificultad del proyecto en términos técnicos, organizativos o de integración con otros sistemas.'),
+('Aspectos Financieros', 'Riesgos relacionados con la disponibilidad y gestión de recursos financieros, afectando la viabilidad y sostenibilidad del proyecto.'),
+('Subcontratistas', 'Riesgos derivados de la dependencia de terceros para la ejecución de partes del proyecto, lo que puede afectar la calidad, tiempos y costos.');
 
 insert into perfil (nombre) VALUES ("Administrador"), ("Usuario Estandar"), ("Espectador");
 INSERT into usuario (nombre, email) VALUES ("Hugo Frey", "hugofrey202@gmail.com"), ("Hugo Antonio Frey", "hugoantoniofrey2003@gmail.com"), ("Agustin Collareda","collaredaagustinpmg@gmail.com"), ("ardkrav", "ardkrav@gmail.com"), ("Cintia Hernandez","cintiasod2021@gmail.com"), ("Mijuku", "cintiah378@gmail.com");
