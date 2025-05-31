42647d4ed8bf53efdd8f80e8ea00b0a4eb5c27d2
fxex <hugoantoniofrey2003@gmail.com>
2025-05-31 20:17:54 -0300

    Feat: Se genero un script con datos del proyecto falta terminar

--- Implementación/Entregable VestaRiskManager/datos basicos.sql
+++ Implementación/Entregable VestaRiskManager/datos basicos.sql
@@ -10,6 +10,57 @@ INSERT INTO categoria (nombre, descripcion) VALUES
 ('Aspectos Financieros', 'Riesgos relacionados con la disponibilidad y gestión de recursos financieros, afectando la viabilidad y sostenibilidad del proyecto.'),
 ('Subcontratistas', 'Riesgos derivados de la dependencia de terceros para la ejecución de partes del proyecto, lo que puede afectar la calidad, tiempos y costos.');
 
-insert into perfil (nombre) VALUES ("Administrador"), ("Usuario Estandar"), ("Espectador");
-INSERT into usuario (nombre, email) VALUES ("Hugo Frey", "hugofrey202@gmail.com"), ("Hugo Antonio Frey", "hugoantoniofrey2003@gmail.com"), ("Agustin Collareda","collaredaagustinpmg@gmail.com"), ("ardkrav", "ardkrav@gmail.com"), ("Cintia Hernandez","cintiasod2021@gmail.com"), ("Mijuku", "cintiah378@gmail.com");
-INSERT INTO usuario_perfil(id_usuario, id_perfil) VALUES (1,1), (2,2), (3,1), (4,2), (5,1), (6,2);
+insert into perfil (nombre) VALUES 
+("Administrador"), 
+("Usuario Estandar"), 
+("Espectador");
+
+INSERT into usuario (nombre, email) VALUES 
+("Hugo Frey", "hugofrey202@gmail.com"), ("Hugo Antonio Frey", "hugoantoniofrey2003@gmail.com"),  
+("ardkrav", "ardkrav@gmail.com"), ("Agustin Collareda","collaredaagustinpmg@gmail.com"), 
+("Mijuku", "cintiasod2021@gmail.com"), ("Cintia Hernandez","cintiah378@gmail.com");
+
+INSERT INTO usuario_perfil(id_usuario, id_perfil) VALUES 
+(1,1), (2,2), 
+(3,1), (4,2), 
+(5,1), (6,2);
+
+Insert Into proyecto (nombre, descripcion, estado, fecha_inicio) values
+("Vesta Risk Manager", "Es una aplicación que optimiza la identificación, analisis y monitereo de los diferentes 
+riesgos que se presentan a la hora de desarrollar un software con el fin de asistir al usuario en la toma de 
+decisiones y mitigar los riesgos", "activo", "2024-08-24");
+
+insert into proyecto_participante (id_proyecto, id_usuario, rol) values
+(1, 2, "Desarrollador"), 
+(1, 6, "Desarrollador"),
+(1, 4, "Lider del Proyecto");
+
+insert into proyecto_categoria (id_proyecto, id_categoria) values
+(1,1),
+(1,2),
+(1,3),
+(1,4),
+(1,5),
+(1,6),
+(1,7),
+(1,8),
+(1,9),
+(1,10);
+
+insert into iteracion (nombre, fecha_inicio, fecha_fin, id_proyecto) values 
+("Etapa Inicio - Iteración 1", "2024-08-27", "2024-09-10", 1),
+("Etapa Elaboración - Iteración 1", "2024-09-11", "2024-09-24", 1),
+("Etapa Elaboración - Iteración 2", "2024-09-25", "2024-10-11", 1),
+("Etapa Construcción - Iteración 1", "2024-10-12", "2024-10-29", 1),
+("Etapa Construcción - Iteración 2", "2024-10-30", "2024-11-08", 1),
+("Etapa Construcción - Iteración 3", "2024-11-09", "2024-11-19", 1),
+("Etapa Construcción - Iteración 4", "2025-01-18", "2025-01-27", 1),
+("Etapa Construcción - Iteración 5", "2025-01-28", "2025-02-11", 1),
+("Etapa Construcción - Iteración 6", "2025-03-06", "2025-03-23", 1),
+("Etapa Construcción - Iteración 7", "2025-03-24", "2025-04-06", 1),
+("Etapa Construcción - Iteración 8", "2025-04-24", "2025-05-09", 1),
+("Etapa Construcción - Iteración 9", "2025-05-10", "2025-05-28", 1),
+("Etapa Cierre - Iteración 1", "2025-05-29", "2025-06-12", 1);
+
+insert into riesgo (id_riesgo, descripcion, fecha_creacion, id_categoria, id_proyecto) values
+();
