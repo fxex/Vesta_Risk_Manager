ff7b5f6aa8000b927f5a766db2d01158d1edcfd7
fxex <hugoantoniofrey2003@gmail.com>
2025-09-04 19:22:22 -0300

    Fix: netlify no acepta http

--- Implementación/Entregable VestaRiskManager/Frontend/src/utils/funciones.js
+++ Implementación/Entregable VestaRiskManager/Frontend/src/utils/funciones.js
@@ -1,5 +1,5 @@
 
-export const URL = "http://lds02.uarg.unpa.edu.ar/Vesta";
+export const URL = "/api/";
 
 /**
  * Valida si un correo electrónico tiene un formato válido y pertenece a los dominios permitidos.
